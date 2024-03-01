f47c517e6ffd82a8a0abefa8bb1dc3d043107234
Nicolas Hvisc <nicohvisc@gmail.com>
2024-03-01 13:56:48 +0100

    Add new function addtodolist

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ def readtodolist():
     with open("FileFolder/datafile.txt","r") as  file:
         for line in file:
             print(line, end="\n")
+def addtodolist():
+    with open("FileFolder/datafile.txt","a") as  file:
+        new_content = input("Zadaj novy todolist: ")
+        file.write(new_content + "\n")
+        print("Pridavam novy todolist")
+    readtodolist()
     
 while True:
    valueofinput = input("Zadaj instrukciu, ktoru chces vykonat: ")
    if valueofinput == "r":
         readtodolist()
+   elif valueofinput == "a":
+       addtodolist()
    elif valueofinput == "q":
         print("ukoncujem program")
         break
