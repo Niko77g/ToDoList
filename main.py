58691c25dc3e39bfa6dea0c1d697b3833f2e0e2e
Nicolas Hvisc <nicohvisc@gmail.com>
2024-03-01 13:48:53 +0100

    Add new function readtodolist

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import datetime
 
-def addtodolist():
-    input("Enter some text: ")
+def readtodolist():
+    with open("FileFolder/datafile.txt","r") as  file:
+        for line in file:
+            print(line, end="\n")
+    
+while True:
+   valueofinput = input("Zadaj instrukciu, ktoru chces vykonat: ")
+   if valueofinput == "r":
+        readtodolist()
+   elif valueofinput == "q":
+        print("ukoncujem program")
+        break
+   else: 
+        print("Neplatny vstup!")
